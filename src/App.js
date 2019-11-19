b9499a9a2ceae332a4cf56aec1cefe2f6f20b9b2
szucsdani <szucsdaniel97@gmail.com>
2019-11-19 15:16:21 +0100

    Full responsive

--- src/App.js
+++ src/App.js
@@ -4,10 +4,21 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App Flex-container">
-      <div className="lefty"></div>
-      <div className="App-box"></div>
-      <div className="righty"></div>
+    <div className="app flex-container">
+      <div className="app-box">
+        <div className="button-container">
+          <button className="button-look">q</button>
+          <button className="button-look">w</button>
+          <button className="button-look">e</button>
+          <button className="button-look">a</button>
+          <button className="button-look">s</button>
+          <button className="button-look">d</button>
+          <button className="button-look">z</button>
+          <button className="button-look">x</button>
+          <button className="button-look">c</button>
+        </div>
+        <div className="function-container"></div>
+      </div>
     </div>
   );
 }
---
+++ src/DrumPadButton.js
@@ -0,0 +1,5 @@
+import React, { useState } from "react";
+import { Button } from "reactstrap";
+import "./App.css";
+
+function DrumPadButton() {}
